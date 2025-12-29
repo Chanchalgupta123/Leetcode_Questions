5e6f91e829e9d5d027af90a08a6e2405eb71085e
Chanchal Gupta <151851382+Chanchalgupta123@users.noreply.github.com>
2025-12-29 22:21:36 +0530

    Add files via upload

---
+++ palindromeNumber.java
@@ -0,0 +1,17 @@
+package LeetCode_Questions;
+
+public class palindromeNumber {
+    class Solution {
+        public boolean isPalindrome(int x) {
+            int sum=0;
+            int target=x;
+            while(x>0){
+                int temp=x % 10;
+                x=x/10;
+                sum=sum*10+temp;
+            }
+            return sum == target ;
+
+        }
+    }
+}
