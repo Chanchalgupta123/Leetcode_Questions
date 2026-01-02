3f04b3780a6332ea0b1ed33e50ca368e11056762
Chanchal Gupta <151851382+Chanchalgupta123@users.noreply.github.com>
2026-01-02 15:58:59 +0530

    Add files via upload

---
+++ N_Repeated_Element_2NArray.java
@@ -0,0 +1,18 @@
+package LeetCode_Questions;
+
+import java.util.HashSet;
+
+public class N_Repeated_Element_2NArray {
+    class Solution {
+        public int repeatedNTimes(int[] nums) {
+            HashSet<Integer> set=new HashSet<>();
+            for(int num:nums){
+                if(set.contains(num)){
+                    return num;
+                }
+                set.add(num);
+            }
+            return -1;
+        }
+    }
+}
